32bdfe0a11aaafa9237a7a0a73c4a820442239ff
EC2 Default User <ec2-user@ip-172-31-41-232.us-west-2.compute.internal>
2023-04-25 23:01:27 +0000

    gcn-lpa

---
+++ src/adaptiveLP.py
@@ -0,0 +1,62 @@
+from typing import Callable, Optional
+import torch
+from torch import Tensor
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.nn.conv.gcn_conv import gcn_norm
+from torch_geometric.typing import Adj, OptTensor, SparseTensor
+from torch_geometric.utils import one_hot, spmm
+
+
+class AdaptiveLabelPropagation(MessagePassing):
+    def __init__(self, num_layers: int, alpha: float):
+        super().__init__(aggr='add')
+        self.num_layers = num_layers
+        self.alpha = alpha
+
+    @torch.no_grad()
+    def forward(
+        self,
+        y: Tensor,
+        edge_index: Adj,
+        mask: OptTensor = None,
+        edge_weight: OptTensor = None,
+        # post_step: Optional[Callable[[Tensor], Tensor]] = None,
+    ) -> Tensor:
+        if y.dtype == torch.long and y.size(0) == y.numel():
+            y = one_hot(y.view(-1))
+
+        out = y
+        if mask is not None:
+            out = torch.zeros_like(y)
+            out[mask] = y[mask]
+
+        if isinstance(edge_index, SparseTensor) and not edge_index.has_value():
+            edge_index = gcn_norm(edge_index, add_self_loops=False)
+        elif isinstance(edge_index, Tensor) and edge_weight is None:
+            edge_index, edge_weight = gcn_norm(edge_index, num_nodes=y.size(0),
+                                               add_self_loops=False)
+
+        self.outs = [out]
+        res = (1 - self.alpha) * out
+        for _ in range(self.num_layers):
+            # propagate_type: (y: Tensor, edge_weight: OptTensor)
+            out = self.propagate(edge_index, x=out, edge_weight=edge_weight,
+                                 size=None)
+            out.mul_(self.alpha).add_(res)
+            self.outs.append(out)
+            # if post_step is not None:
+            #     out = post_step(out)
+            # else:
+            #     out.clamp_(0., 1.)
+            
+        return self.outs
+
+    def message(self, x_j: Tensor, edge_weight: OptTensor) -> Tensor:
+        return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
+
+    def message_and_aggregate(self, adj_t: SparseTensor, x: Tensor) -> Tensor:
+        return spmm(adj_t, x, reduce=self.aggr)
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}(num_layers={self.num_layers}, '
+                f'alpha={self.alpha})')
--- src/models.py
+++ src/models.py
@@ -1,7 +1,8 @@
 import torch
 from torch import Tensor
-from torch_geometric.nn import GCNConv, GATConv
+from torch_geometric.nn import GCNConv, GATConv, LabelPropagation
 import torch.nn.functional as F
+from alp import AdaptiveLabelPropagation
 
 class GCN(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels):
@@ -36,3 +37,70 @@ class GAT(torch.nn.Module):
         # self.s = self.alpha * self.s + (1 - self.alpha) * temp_S
         # w = self.s * xtx
         return x
+
+class LP(torch.nn.Module):
+    def __init__(self, num_layers, alpha):
+        super().__init__()
+        self.prop = LabelPropagation(num_layers, alpha)
+    
+    def train(self, dataset):
+        self.data = dataset[0]
+        self.out = self.prop(self.data.y, self.data.edge_index, self.data.train_mask)
+        return self.out
+    
+    def test(self):
+        pred = self.out.argmax(dim=-1)
+        accs = []
+        for mask in [self.data.train_mask, self.data.val_mask, self.data.test_mask]:
+            accs.append(int((pred[mask] == self.data.y[mask]).sum()) / int(mask.sum()))
+        return accs
+
+class ALP(torch.nn.Module):
+    def __init__(self, num_layers, alpha, yshape):
+        super().__init__()
+        self.prop = AdaptiveLabelPropagation(num_layers, alpha)
+        self.yshape = yshape
+        self.weight1 = torch.nn.Linear(yshape, yshape, bias=True)
+        self.weight2 = torch.nn.Linear(yshape, 1, bias=False)
+    
+    def lpa(self, dataset):
+        self.data = dataset[0]
+        
+        sub_train_mask = self.data.train_mask.clone()
+        # sub_train_mask[torch.randperm(sub_train_mask.shape[0])[:int(sub_train_mask.shape[0] * 0.7)]] = False
+        # print(sub_train_mask.sum(), self.data.train_mask.sum())
+        
+        self.outs = self.prop(self.data.y, self.data.edge_index, sub_train_mask)
+        # print(self.outs[0].shape)
+        self.outs = torch.stack(self.outs).permute(0, 2, 1)
+        # print(self.outs.shape)
+        return self.outs
+    
+    def forward(self):
+        q = self.weight1(self.outs)
+        q = torch.relu(q)
+        alpha = self.weight2(q)
+        alpha = torch.nn.Softmax(dim=-1)(alpha.view(self.outs.shape[1], -1))
+        alpha = alpha.view(self.outs.shape[1], -1, 1).float()
+        b = torch.sum(alpha * self.outs.permute(1, 0, 2), dim=1).permute(1, 0)
+        # b = torch.nn.Sof .tmax(dim=-1)(b)
+        return b
+    
+    def predict(self):
+        pred = self.forward().argmax(dim=-1)
+        return pred
+    
+    # def loss(self):
+    #     pred = self.forward()[self.data.train_mask]
+    #     y = self.data.y[self.data.train_mask]
+    #     # y = torch.nn.functional.one_hot(y).float()
+    #     # print(y.shape)
+    #     # print(pred.sum(axis=1), y.sum(axis=1))
+    #     return torch.nn.CrossEntropyLoss()(pred, y)
+        
+    def test(self):
+        pred = self.predict()
+        accs = []
+        for mask in [self.data.train_mask, self.data.val_mask, self.data.test_mask]:
+            accs.append(int((pred[mask] == self.data.y[mask]).sum()) / int(mask.sum()))
+        return accs
