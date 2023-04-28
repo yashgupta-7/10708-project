9733989d43a4b964a9def9abea6d489cad2f1d5a
yashgupta-7 <yashgupta1427@gmail.com>
2023-04-28 11:05:02 -0400

    added gcnlpa

--- src/models.py
+++ src/models.py
@@ -56,6 +56,42 @@ class LP(torch.nn.Module):
             accs.append(int((pred[mask] == self.data.y[mask]).sum()) / int(mask.sum()))
         return accs
 
+class AdaptiveLP(torch.nn.Module):
+    def __init__(self, num_layers, yshape, edge_dim):
+        super().__init__()
+        self.k = num_layers
+        self.yshape = yshape
+        self.edge_weight = torch.nn.Parameter(torch.ones(edge_dim))
+        self.weight1 = torch.nn.Linear(yshape, yshape, bias=True)
+        self.weight2 = torch.nn.Linear(yshape, 1, bias=False)
+    
+    def dense_lp(self, data):
+        labels = torch.nn.functional.one_hot(data.y.type(torch.long)).type(torch.float)
+        labels[~data.train_mask] = 0
+        matrix = torch_geometric.utils.to_dense_adj(data.edge_index, edge_attr=self.edge_weight.sigmoid(), max_num_nodes=data.num_nodes)
+        matrix = matrix.squeeze(0)
+        selfloop = torch.diag(torch.ones(matrix.shape[0]))
+        matrix += selfloop
+        self.ys = [labels]
+        for _ in range(self.k):
+          y = torch.matmul(matrix, labels)
+          self.ys.append(y)
+          labels = y
+        return torch.nn.functional.normalize(labels, dim=1)
+    
+    def forward(self, data):
+        dense_lp = self.dense_lp(data)
+        # return dense_lp
+        self.ys = torch.stack(self.ys).permute(0, 2, 1)
+        q = self.weight1(self.ys)
+        q = torch.relu(q)
+        alpha = self.weight2(q)
+        alpha = torch.nn.Softmax(dim=-1)(alpha.view(self.ys.shape[1], -1))
+        alpha = alpha.view(self.ys.shape[1], -1, 1).float()
+        b = torch.sum(alpha * self.ys.permute(1, 0, 2), dim=1).permute(1, 0)
+        return torch.nn.functional.normalize(b, dim=1)
+    
+        
 class ALP(torch.nn.Module):
     def __init__(self, num_layers, alpha, yshape):
         super().__init__()
@@ -90,15 +126,7 @@ class ALP(torch.nn.Module):
     def predict(self):
         pred = self.forward().argmax(dim=-1)
         return pred
-    
-    # def loss(self):
-    #     pred = self.forward()[self.data.train_mask]
-    #     y = self.data.y[self.data.train_mask]
-    #     # y = torch.nn.functional.one_hot(y).float()
-    #     # print(y.shape)
-    #     # print(pred.sum(axis=1), y.sum(axis=1))
-    #     return torch.nn.CrossEntropyLoss()(pred, y)
-        
+            
     def test(self):
         pred = self.predict()
         accs = []
